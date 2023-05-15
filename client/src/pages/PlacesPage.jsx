fdf92192f575ec3da848bf02fc8223c5e68ebf9f
Priyanshu <naveragec0der@gmail.com>
2023-05-16 03:48:57 +0530

    final changes for deployment

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -37,7 +37,7 @@ export default function PlacesPage() {
       <div className="mt-4">
         {places.length>0 && places.map((place,index)=>(
           <Link to={'/account/places/'+place._id} key={index} className="flex cursor-pointer gap-4 bg-gray-200 p-4 rounded-2xl">
-            <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
+            <div className="flex w-32 h-32 bg-gray-300 shrink-0">
               <PlaceImg place={place} />
             </div> 
             <div className="grow-0 shrink"> 
